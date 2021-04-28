8c010a6c7ecbb75ee1873b6d279c74fa77ce4477
Juan Embid <78643148+Juan-Embid@users.noreply.github.com>
2021-04-28 18:55:51 +0200

    Botones funcionan
    Implementado boton

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,1 +1,18 @@
+from tkinter import *
+from tkinter import filedialog
+import subprocess, os
 
+root = Tk()
+
+def browseFiles():
+    filedialog.askopenfilename(initialdir = "", title = "Select a File", filetypes = (("all files", "*.*"), ("Text files", "*.txt*")))
+def openCamera():
+  os.system('cmd /c "start microsoft.windows.camera:"')
+
+browse = Button(root, text = "Browse files", command = browseFiles)
+browse.pack()
+camera = Button(root, text = "Open camera", command = openCamera)
+camera.pack()
+root.title("BlurApp")
+root.geometry("600x300")
+root.mainloop()
