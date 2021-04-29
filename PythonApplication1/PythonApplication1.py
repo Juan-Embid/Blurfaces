48b931e37cb1470dfa422e9c962d871abb84b67f
Juan Embid <78643148+Juan-Embid@users.noreply.github.com>
2021-04-29 17:47:54 +0200

    Acabado (a medias)
    hecho que borre caras en fotos y videos, pero no está implementado el botón de guardar ni la función de que te borre la cara en vivo

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,8 +1,8 @@
 from tkinter import *
 from tkinter import filedialog
 import os
-import cv2 as cv
-import numpy as np
+import cv2
+from tkinter.filedialog import asksaveasfile
 
 root = Tk()
 
@@ -11,37 +11,52 @@ def rescaleFrame(frame, scale = 0.75): #rescala tanto los videos como las imagen
     height = int(frame.shape[0] * scale)
     dimensions = (width, height)
 
-    return cv.resize(frame, dimensions, interpolation = cv.INTER_AREA)
+    return cv2.resize(frame, dimensions, interpolation = cv2.INTER_AREA)
 
 def browseFiles():
     file = filedialog.askopenfilename(initialdir = "", title = "Select a File", filetypes = (("all files", "*.*"), ("Text files", "*.txt*"))) #Pide el archivo
 
     if file.lower().endswith(('.mp4')): #Abre solo los videos
-      capture = cv.VideoCapture(file)
+      cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+      capture = cv2.VideoCapture(file)
+
+      def find_blur(bw, color):
+        faces = cascade.detectMultiScale(bw, 1.1, 7)
+
+        for (x, y, w, h) in faces:
+          #cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness = 2)
+          blur = color[y:y+h, x:x+w] #selecciona las caras
+          gauss = cv2.GaussianBlur(blur, (101,101), 0) #Difumina la cara
+          color[y:y+h, x:x+w] = gauss #vuelve a pegar la cara en la imagen
+
+        return color
 
       while True:
-        istrue, frame = capture.read()
-
-        if type(frame) is type(None): #para que no salte el error -250
-            break
-        haar_cascade = cv.CascadeClassifier('haar_face.xml')
-        faces_rect = haar_cascade.detectMultiScale(frame, scaleFactor = 1.1, minNeighbors = 8)
-        for (x, y, w, h) in faces_rect: #Comentar si no quieres que salga el recuadro al rededor de la cara
-          cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), thickness = 2)
-          cv.imshow('https://github.com/Juan-Embid/Blurfaces', rescaleFrame(frame))
-      
-        if cv.waitKey(20) & 0xFF==ord('d'):
-            break
+        _, color = capture.read()
+        if type(color) is type(None): #para que no salte el error -250
+          break
+        bw = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
+        cara = find_blur(bw, color)
+        cv2.imshow('https://github.com/Juan-Embid/Blurfaces', rescaleFrame(cara))
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+          break
 
     if file.lower().endswith(('.jpg', '.png', 'jfif')): #Abre solo las imagenes
-      img = cv.imread(file)
-      haar_cascade = cv.CascadeClassifier('haar_face.xml')
-      faces_rect = haar_cascade.detectMultiScale(img, scaleFactor = 1.1, minNeighbors = 8)
+      cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-      for (x, y, w, h) in faces_rect: #Comentar si no quieres que salga el recuadro al rededor de la cara
-          cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness = 2)
-          average = cv.blur(img, (10,10)) #Difumina la cara
-          cv.imshow('https://github.com/Juan-Embid/Blurfaces', rescaleFrame(img))
+      def find_blur(bw, color):
+        faces = cascade.detectMultiScale(bw, 1.1, 7)
+
+        for (x, y, w, h) in faces:
+          #cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness = 2)
+          blur = img[y:y+h, x:x+w] #selecciona las caras
+          gauss = cv2.GaussianBlur(blur, (101,101), 0) #Difumina la cara
+          img[y:y+h, x:x+w] = gauss #vuelve a pegar la cara en la imagen
+        return color
+      img = cv2.imread(file)
+      bw = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+      cara = find_blur(bw, img)
+      cv2.imshow('https://github.com/Juan-Embid/Blurfaces', rescaleFrame(cara))
 
 def openCamera():
   os.system('cmd /c "start microsoft.windows.camera:"')
