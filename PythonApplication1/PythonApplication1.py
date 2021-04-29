acbd97629cc9985b46b9d2fee8cd56a0ae25167d
Juan Embid <78643148+Juan-Embid@users.noreply.github.com>
2021-04-29 10:44:25 +0200

    nada

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -25,7 +25,7 @@ def browseFiles():
         if type(frame) is type(None): #para que no salte el error -250
             break
         haar_cascade = cv.CascadeClassifier('haar_face.xml')
-        faces_rect = haar_cascade.detectMultiScale(frame, scaleFactor = 1.1, minNeighbors = 10)
+        faces_rect = haar_cascade.detectMultiScale(frame, scaleFactor = 1.1, minNeighbors = 8)
         for (x, y, w, h) in faces_rect: #Comentar si no quieres que salga el recuadro al rededor de la cara
           cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), thickness = 2)
           cv.imshow('https://github.com/Juan-Embid/Blurfaces', rescaleFrame(frame))
@@ -33,16 +33,15 @@ def browseFiles():
         if cv.waitKey(20) & 0xFF==ord('d'):
             break
 
-    if file.lower().endswith(('.jpg', '.png')): #Abre solo las imagenes
+    if file.lower().endswith(('.jpg', '.png', 'jfif')): #Abre solo las imagenes
       img = cv.imread(file)
       haar_cascade = cv.CascadeClassifier('haar_face.xml')
-      faces_rect = haar_cascade.detectMultiScale(img, scaleFactor = 1.1, minNeighbors = 10)
+      faces_rect = haar_cascade.detectMultiScale(img, scaleFactor = 1.1, minNeighbors = 8)
 
       for (x, y, w, h) in faces_rect: #Comentar si no quieres que salga el recuadro al rededor de la cara
           cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness = 2)
           average = cv.blur(img, (10,10)) #Difumina la cara
-          cv.imshow('https://github.com/Juan-Embid/Blurfaces', rescaleFrame(average))
-
+          cv.imshow('https://github.com/Juan-Embid/Blurfaces', rescaleFrame(img))
 
 def openCamera():
   os.system('cmd /c "start microsoft.windows.camera:"')
